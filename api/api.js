f1d1bc7510a1a2792669431fd983e5d2e6a2d9b4
n2qv <n2qv1994@gmail.com>
2016-07-26 21:12:29 +0700

    add router

--- api/api.js
+++ api/api.js
@@ -1,7 +1,7 @@
 var express = require('express');
+var taskController = require('../controllers/taskcontroller.js');
 var router = express.Router();
 
-
-
+router.post('/postingTask', taskController.postingTask);
 
 module.exports = router;
