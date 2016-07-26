3309789c116fbb87dc06f4c07f5de0ca0dba8c37
AnhTu18294 <tu.na.1821994@gmail.com>
2016-07-26 11:43:33 +0700

    connect database has done!

--- app.js
+++ app.js
@@ -2,19 +2,36 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var api = require('./api/api');
-var databaseService = require('./db/database.service');
+var dbConfig = require('./config/database.config.json');
+var mongoService = require('./db/mongo.service');
+
+var connectionString = "mongodb://"+dbConfig.mongodb.host+":"+dbConfig.mongodb.port+"/"+dbConfig.mongodb.dbname;
+
 
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 
+// set api
+app.use('/api', api);
 // set route link to home.html
 app.get('/home', function(req, res){
 	res.sendFile(__dirname + "/views/home.html");
 });
 
-// starting app
-app.listen(app.get('port'), function(){
-	console.log("application started on http://localhost:"+app.get('port')+";\n please press Ctrl+C to terminate");
-}) 
+// start connection mongodb and starting app
+
+mongoService.connect(connectionString, function(err){
+	console.log('connection string:\n' + connectionString);
+	if(err){
+		console.log('Unable to connect Mongo DB');
+		process.exit(1);
+	} else{
+		console.log(mongoService.getConnection());
+		app.listen(app.get('port'), function(){
+			console.log("application started on http://localhost:"+app.get('port')+";\n please press Ctrl+C to terminate");
+		});
+	}
+});
+	
