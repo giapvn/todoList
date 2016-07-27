d7d6f82a18f117945ad69c99a66055e176ae6cd9
n2qv <n2qv1994@gmail.com>
2016-07-27 09:06:00 +0700

    geter, seter for task

--- models/task.js
+++ models/task.js
@@ -1,1 +1,20 @@
 'use strict'
+function Task(){};
+
+Task.prototype.setDesciption = function(description){
+	this.description = description;
+};
+
+// Task.prototype.setActivationTime =function(activation_time){
+// 	this.activation_time = activation_time;
+// };
+
+Task.prototype.getDesciption = function(){
+	return this.description;
+};
+
+// Task.prototype.getActivationTime = function(){
+// 	return this.activation_time;
+// };
+
+module.exports = Task;
